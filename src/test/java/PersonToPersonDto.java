0aa620fc0ef24e3d8834eec3517b0a434c33eb61
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-06-27 21:49:21 +0200

    mapping examples

--- src/test/java/PersonToPersonDto.java
+++ src/test/java/PersonToPersonDto.java
@@ -1,8 +1,8 @@
 import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 /**
  * Created by mtumilowicz on 2018-06-27.
@@ -11,10 +11,12 @@ class PersonToPersonDto {
     @Test
     void map() {
         ModelMapper modelMapper = new ModelMapper();
-        TypeMap<Person, PersonDto> typeMap = modelMapper.createTypeMap(Person.class, PersonDto.class)
+
+        modelMapper.createTypeMap(Person.class, PersonDto.class)
                 .addMapping(Person::getName, PersonDto::setFirstName)
                 .addMapping(x -> x.getAddress().getCity(), PersonDto::setCity)
                 .addMapping(x -> x.getAddress().getStreet(), PersonDto::setStreet);
+
 
         PersonDto personDto = modelMapper.map(Person.builder()
                         .name("Michal")
@@ -22,9 +24,33 @@ class PersonToPersonDto {
                         .address(new Address("Warsaw", "Nowy Swiat"))
                         .build(),
                 PersonDto.class);
+
         assertEquals("Michal", personDto.getFirstName());
         assertEquals(15, personDto.getAge());
         assertEquals("Warsaw", personDto.getCity());
         assertEquals("Nowy Swiat", personDto.getStreet());
+    }
+
+    @Test
+    void map2() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.createTypeMap(Person.class, PersonDto.class)
+                .addMapping(Person::getName, PersonDto::setFirstName)
+                .addMapping(x -> x.getAddress().getCity(), PersonDto::setCity)
+                .addMapping(x -> x.getAddress().getStreet(), PersonDto::setStreet);
+
+
+        PersonDto personDto = modelMapper.map(Person.builder()
+                        .name("Michal")
+                        .age(15)
+                        .address(null)
+                        .build(),
+                PersonDto.class);
+
+        assertEquals("Michal", personDto.getFirstName());
+        assertEquals(15, personDto.getAge());
+        assertNull(personDto.getCity());
+        assertNull(personDto.getStreet());
     }
 }
